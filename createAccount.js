7e0b6687cad8efec7a61c24c3787226da0dd37eb
Timothy J McGuire <tmcguire@mcode.com>
2022-06-02 22:29:33 -0500

    Initial upload while building MIT example.
    Components just started from MicroCODE Template.

---
+++ createAccount.js
@@ -0,0 +1,191 @@
+// #region  H E A D E R
+// <copyright file="createAccount.js" company="MicroCODE Incorporated">Copyright © 2022 MicroCODE, Inc. Troy, MI</copyright><author>Timothy J. McGuire</author>
+// #region  P R E A M B L E
+// #region  D O C U M E N T A T I O N
+/*
+ *      Title:    MicroCODE Bad Bank React Create Account
+ *      Module:   Modules (./createAccount.js)
+ *      Project:  MicroCODE Bad Bank React App
+ *      Customer: Internal
+ *      Creator:  MicroCODE Incorporated
+ *      Date:     June 2022
+ *      Author:   Timothy J McGuire
+ *
+ *      Designed and Coded: 2022 MicroCODE Incorporated
+ *
+ *      This software and related materials are the property of
+ *      MicroCODE Incorporated and contain confidential and proprietary
+ *      information. This software and related materials shall not be
+ *      duplicated, disclosed to others, or used in any way without the
+ *      written of MicroCODE Incorported.
+ *
+ *
+ *      DESCRIPTION:
+ *      ------------
+ *
+ *      This module implements the MicroCODE's Bad Bank React Create Account.
+ *
+ *
+ *      REFERENCES:
+ *      -----------
+ *
+ *      1. MicroCODE JavaScript Style Guide
+ *         Local File: MCX-S02 (Internal JS Style Guide).docx
+ *         https://github.com/MicroCODEIncorporated/JavaScriptSG
+ *
+ *
+ *
+ *      DEMONSTRATION VIDEOS:
+ *      --------------------
+ *
+ *      1. ...
+ *
+ *
+ *
+ *      MODIFICATIONS:
+ *      --------------
+ *
+ *  Date:         By-Group:   Rev:     Description:
+ *
+ *  02-Jun-2022   TJM-MCODE  {0001}    New module implementing the Bad Bank Create Account.
+ *
+ *
+ */
+"use strict";
+
+// #endregion
+// #endregion
+// #endregion
+
+// #region  J A V A S C R I P T
+// #region  F U N C T I O N S
+
+// #region  C O N S T A N T S
+
+const TIMEOUT_MSEC = 3000;
+const MINIMUM_OPENING_DEPOSIT = 100;
+
+// #endregion
+
+// #region  P R I V A T E   F I E L D S
+
+// #endregion
+
+// #region  E N U M E R A T I O N S
+
+// #endregion
+
+// #region  C O M P O N E N T – P U B L I C
+
+/**
+ * CreateAccount() – the Bad Bank Create Account Component.
+ *
+ * @api public
+ *
+ * @param {null} no properties.
+ *
+ * @returns JavaScript Extension (JSX) code representing the current state of the component.
+ *
+ * @example
+ *
+ *      CreateAccount();
+ *
+ */
+function CreateAccount()
+{
+    // valid PROPS input(s)
+
+    // initialize STATE and define acessors...
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState('');
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    // access CONTEXT for reference...
+    const ctx = React.useContext(UserContext);
+
+    // #region  P R I V A T E   F U N C T I O N S
+
+    // field validation...
+    function validate(field, label)
+    {
+        if (!field)
+        {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''), TIMEOUT_MSEC);
+            return false;
+        }
+        return true;
+    }
+
+    // #endregion
+
+    // #region  E V E N T   H A N D L E R S
+    /*
+     * *_Click() - 'on click' event handlers for UI elements.
+     */
+
+    // creates a User Account if passed validate input fields
+    function handleCreate_Click()
+    {
+        console.log(name, email, password);
+        if (!validate(name, 'name')) return;
+        if (!validate(email, 'email')) return;
+        if (!validate(password, 'password')) return;
+        ctx.users.push({name, email, password, balance: MINIMUM_OPENING_DEPOSIT});
+        setShow(false);
+    }
+
+    // clears the UI fields for Account creation unconditionally
+    function clearForm_Click()
+    {
+        setName('');
+        setEmail('');
+        setPassword('');
+        setShow(true);
+    }
+
+    // #endregion
+
+    // perform component COMPUTATION to generate output
+
+    // OUTPUT the Component's JavaScript Extension (JSX) code...
+    return (
+        <Card
+            bgcolor="primary"
+            header="Create Account"
+            status={status}
+            body={show ? (
+                <>
+                    Name<br />
+                    <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br />
+                    Email Address<br />
+                    <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br />
+                    Password<br />
+                    <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br />
+                    <button type="submit" className="btn btn-light" onClick={handleCreate_Click}>Create Account</button>
+                </>
+            ) : (
+                <>
+                    <h5>Success</h5>
+                    <button type="submit" className="btn btn-light" onClick={clearForm_Click}>Add another account</button>
+                </>
+            )}
+        />
+    );
+}
+
+// #endregion
+
+// #region  C O M P O N E N T - E X P O R T S
+
+if (typeof module !== 'undefined')
+{
+    module.exports = {CreateAccount};
+}
+
+// #endregion
+
+// #endregion
+// #endregion
---
+++ navbar.js
@@ -0,0 +1,148 @@
+// #region  H E A D E R
+// <copyright file="navbar.js" company="MicroCODE Incorporated">Copyright © 2022 MicroCODE, Inc. Troy, MI</copyright><author>Timothy J. McGuire</author>
+// #region  P R E A M B L E
+// #region  D O C U M E N T A T I O N
+/*
+ *      Title:    MicroCODE Bad Bank React Nav Bar
+ *      Module:   Modules (./navbar.js)
+ *      Project:  MicroCODE Bad Bank React App
+ *      Customer: Internal
+ *      Creator:  MicroCODE Incorporated
+ *      Date:     June 2, 2022
+ *      Author:   Timothy J McGuire
+ *
+ *      Designed and Coded: 2022 MicroCODE Incorporated
+ *
+ *      This software and related materials are the property of
+ *      MicroCODE Incorporated and contain confidential and proprietary
+ *      information. This software and related materials shall not be
+ *      duplicated, disclosed to others, or used in any way without the
+ *      written of MicroCODE Incorported.
+ *
+ *
+ *      DESCRIPTION:
+ *      ------------
+ *
+ *      This module implements the MicroCODE's Bad Bank React Nav Bar.
+ *
+ *
+ *      REFERENCES:
+ *      -----------
+ *
+ *      1. MicroCODE JavaScript Style Guide
+ *         Local File: MCX-S02 (Internal JS Style Guide).docx
+ *         https://github.com/MicroCODEIncorporated/JavaScriptSG
+ *
+ *
+ *
+ *      DEMONSTRATION VIDEOS:
+ *      --------------------
+ *
+ *      1. ...
+ *
+ *
+ *
+ *      MODIFICATIONS:
+ *      --------------
+ *
+ *  Date:         By-Group:   Rev:     Description:
+ *
+ *  02-Jun-2022   TJM-MCODE  {0001}    New module implementing the Bad Bank Nav Bar.
+ *
+ *
+ */
+"use strict";
+
+// #endregion
+// #endregion
+// #endregion
+
+// #region  J A V A S C R I P T
+// #region  F U N C T I O N S
+
+// #region  C O N S T A N T S
+
+// #endregion
+
+// #region  P R I V A T E   F I E L D S
+
+// #endregion
+
+// #region  E N U M E R A T I O N S
+
+// #endregion
+
+// #region  C O M P O N E N T – P U B L I C
+
+/**
+ * NavBar() – the Bad Bank Navigation Bar.
+ *
+ * @api public
+ *
+ * @param {null} no properties.
+ *
+ * @returns JavaScript Extension (JSX) code representing the current state of the component.
+ *
+ * @example
+ *
+ *      NavBar();
+ *
+ */
+function NavBar()
+{
+    // valid PROPS input(s)
+
+    // initialize STATE and define acessors...
+
+    // valid CONTEXT if required
+
+    // perform component COMPUTATION to generate output
+
+    // OUTPUT the Component's JavaScript Extension (JSX) code...
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">BadBank</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/createAccount/">Create Account</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/login/">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/deposit/">Deposit</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/balance/">Balance</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#/alldata/">AllData</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+// #endregion
+
+// #region  C O M P O N E N T - E X P O R T S
+
+if (typeof module !== 'undefined')
+{
+    module.exports = {NavBar};
+}
+
+// #endregion
+
+// #endregion
+// #endregion
